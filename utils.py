93ffafe1585174262115eb2d59a1935b3ff7d718
Nikola Tonev <koko.tonev@gmail.com>
2019-03-12 13:04:59 +0000

    Utils update

--- utils.py
+++ utils.py
@@ -1,6 +1,43 @@
+# Author: Nikola Tonev
+# Date: March 2019
+
 import os
 from scipy import signal
+from config import hparams
 import numpy as np
+import tensorflow as tf
+
+
+# Computing the actual sequence lengths without the padding for all examples in a batch
+def compute_seq_lens(input):
+
+	# Keep only the highest frequency measurement for each time step --> flattening the 3rd dimension; convert to 1s and 0s
+	used = tf.sign(tf.reduce_max(tf.abs(input), 2))
+
+	# Sum all time steps where there is some frequency info (value is 1, not 0) --> flattening the 2nd dimension and giving an array of sequence lengths for all examples in the batch.
+	lengths = tf.reduce_sum(used, 1)
+	lengths = tf.cast(lengths, tf.int32)
+
+	return lengths
+
+
+# Padding all sequences in a batch, so they are all equal length (equal to the longest one)
+def pad_sequences(sequences, max_len):
+
+	padded_sequences = []
+
+	for seq in sequences:
+		# Creating zero-filled vectors of length n_features to make all sequences equal in length
+		padding = [zeros(len(seq[1])) for _ in range(max_len - len(seq))]
+
+		if len(padding) > 0:
+			padded_sequences.append(np.concatenate((seq, padding), axis=0))
+		else:
+			padded_sequences.append(seq)
+
+	# Will return a tensor of shape (batch_size, max_len, n_features)
+	return padded_sequences 
+
 
 
 def load_output_mapping(dataset):
@@ -57,3 +94,68 @@ def compute_log_linear_spectrogram(audio, sample_rate, window_size=10, step_size
 	feats = (feats - np.mean(feats))/np.std(feats)
 
 	return feats
+
+
+
+
+
+def load_data(dataset, max_data=0, train_size=0.8):
+
+	if dataset == 'librispeech':
+
+		# Defining the paths to the train and tests sets for LibriSpeech
+		train_path = os.path.join(hparams.librispeech_path, 'train-clean-100')
+		test_path = os.path.join(hparams.librispeech_path, 'test-clean')
+
+		# Defininf the max data for training and testing
+		max_data_train = int(max_data*train_size)
+		max_data_test = int(max_data*(1-train_size))
+
+		train_data = []
+		test_data = []
+
+		# Loading training data
+		for file in os.listdir(train_path):
+			
+			# Limiting the max number of files that will be loaded.
+			if max_data > 0 and len(train_data) >= int(max_data_train):
+				train_data = train_data[:max_data_train]
+				break
+
+			arr = np.load(os.path.join(train_path, file))
+			audio = arr[0]
+			label = arr[1]
+			train_data.append((audio, label))
+		
+
+		# Loading test data
+		for file in os.listdir(test_path):
+
+			# Limiting the max number of files that will be loaded.
+			if max_data > 0 and len(test_data) >= int(max_data_test):
+				test_data = test_data[:max_data_test]
+				break
+
+			arr = np.load(os.path.join(test_path, file))
+			audio = arr[0]
+			label = arr[1]
+			test_data.append((audio, label)) 
+
+
+		train_audio = [x[0] for x in train_data]
+		train_labels = [x[1] for x in train_data]
+		test_audio = [x[0] for x in test_data]
+		test_labels = [x[1] for x in test_data]
+
+		return train_audio, train_labels, test_audio, test_labels
+
+
+
+	elif dataset == 'bulphonc':
+
+		# TO DO 
+		pass
+
+
+	else:
+		raise ValueError("Invalid dataset name! Should be either 'librispeech' or 'bulphonc'! ")
