dbfc785e71be1c994da26d3bbfe49d55107e849d
Nikola Tonev <koko.tonev@gmail.com>
2019-04-26 15:24:28 +0300

    Config Update

--- config.py
+++ config.py
@@ -5,27 +5,28 @@ from tensorflow.contrib.training import HParams
 
 hparams = HParams(
 
-	batch_size = 1,
-	n_epochs = 1000,
-	steps_per_checkpoint = 1,
+	batch_size = 32,
+	n_epochs = 20,
+	steps_per_checkpoint = 160,
 	checkpoints_dirpath = 'model/checkpoints',
-	learning_rate = 0.003,
+	learning_rate = 0.025,
 	log_dir='logs',	
 	max_data = 0,
-	load_from_checkpoint = True,
+	load_from_checkpoint = False,
 
 
 	# Data config
-	dataset='librispeech',
+	dataset='speech_commands', # 'librispeech'/'bulphonc'/'speech_commands'
 	librispeech_path='data/librispeech_processed',
 	bulphonc_path='data/bulphonc_processed',
+	speech_commands_path = 'data/speech_commands_processed_reduced',
 
 	# CNN config
 	cnn_layers = 2,
 
 	# RNN config
 	rnn_layers = 3,  # Best: 7 -- possible upgrade
-	rnn_size = 64,
+	rnn_size = 128,
 
 	# Decoder config
 	beam_width = 32
